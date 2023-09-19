baf4d82e1d2e25d58fc38662424052fe1360a1cb
PUNAMRAMPUKALE <144289922+PUNAMRAMPUKALE@users.noreply.github.com>
2023-09-19 01:37:46 -0700

    nth-prime-number-code-added

--- Pascalgenerator.java
+++ Pascalgenerator.java
@@ -1,7 +1,10 @@
 import java.math.BigInteger;
 
 class pascalgenerator {
-//Method to iterate rows
+
+    //----------------------  PascalTriangleGenerator  ------------------ 
+
+    //Method to iterate rows
     int[][] generateTriangle(int rows) {
         int[][] triangle = new int[rows][];
         for (int i = 0; i < rows; i++) {
@@ -19,22 +22,72 @@ class pascalgenerator {
         BigInteger result = BigInteger.ONE;
         for (int i = 0; i < r; i++) {
             result = result.multiply(BigInteger.valueOf(n - i))
-                    .divide(BigInteger.valueOf(i + 1));
+                .divide(BigInteger.valueOf(i + 1));
         }
         return result.intValue();
     }
-    //Main method
+
+
+    // -------------- Given a number n, determine what the nth prime --------
+
+
+    //calculate Nth Prime
+    public int nth(int nth) {
+        if (nth <= 0) {
+            //error handling
+            throw new IllegalArgumentException("Input must be a positive integer.");
+        }
+
+        int count = 0;
+        int number = 2;
+
+        while (true) {
+            if (isPrime(number)) {
+                count++;
+                if (count == nth) {
+                    return number;
+                }
+            }
+            number++;
+        }
+    }
+
+    //recursive function to check if it is a prime number
+    private boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        if (num <= 3) {
+            return true;
+        }
+        if (num % 2 == 0 || num % 3 == 0) {
+            return false;
+        }
+
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    //main method
     public static void main(String[] args) {
         int rows = 5;
         pascalgenerator generator = new pascalgenerator();
         int[][] triangle = generator.generateTriangle(rows);
 
-        // Print Pascal's triangle
+        // Print Pascal's triangle 
         for (int i = 0; i < triangle.length; i++) {
             for (int j = 0; j < triangle[i].length; j++) {
                 System.out.print(triangle[i][j] + " ");
             }
             System.out.println();
         }
+        // Print Nth Prime Number
+        int nthPrime = generator.nth(6);
+        System.out.println("The 6th prime number is: " + nthPrime);
     }
 }
